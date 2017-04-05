9cb55a2281fa14a2b618a3482370a21473cee7c4
Jan Moeller <jan.moeller0@gmail.com>
2017-04-05 06:49:54 +0200

    Add curle-devel; clone docker

--- setup.sh
+++ setup.sh
@@ -1,12 +1,15 @@
 yum groupinstall -y "Development Tools"
-yum install -y gettext-devel openssl-devel perl-CPAN perl-devel zlib-devel
-wget https://github.com/git/git/archive/v2.12.2.tar.gz
+yum install -y gettext-devel openssl-devel perl-CPAN perl-devel zlib-devel curl-devel
+wget https://github.com/git/git/archive/v2.12.2.tar.gz -O git.tar.gz
 tar -zxf git.tar.gz
-cd git-*
+cd git-2.12.2
 make configure
 ./configure --prefix=/usr/local
 make install
+rm -rf git*
 git config --global user.name "Jan Möller"
 git config --global user.email "jan.moeller0@gmail.com"
 
 yum -y install docker
+
+git clone https://github.com/CookingQuest/docker.git
