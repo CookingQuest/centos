e39ab25b58d4be67694ea4d563175dbdb79c7965
Jan Moeller <jan.moeller0@gmail.com>
2017-04-05 07:41:15 +0200

    Improvements

--- setup.sh
+++ setup.sh
@@ -6,10 +6,22 @@ cd git-2.12.2
 make configure
 ./configure --prefix=/usr/local
 make install
+cd ..
 rm -rf git*
 git config --global user.name "Jan Möller"
 git config --global user.email "jan.moeller0@gmail.com"
 
-yum -y install docker
+yum install -y yum-utils
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum-config-manager --enable docker-ce-edge
+yum makecache fast && yum install -y docker-ce
+systemctl enable docker
+systemctl start docker
+
+yum upgrade python*
+yum install -y epel-release python-pip && pip install --upgrade pip
+pip install docker-compose
 
 git clone https://github.com/CookingQuest/docker.git
+
+rm -f setup.sh
